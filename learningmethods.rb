d2fafe8df9a20d1ff2043b100dd2a488c2189ded
Genevieve Uy <workgenevieve@gmail.com>
2012-02-13 15:45:48 -0800

    added zoo_names method

--- learningmethods.rb
+++ learningmethods.rb
@@ -1,10 +1,20 @@
+#this is the array where one element will be pass to the zoo poop report as args
+#animal
 animals = ["giraffe", "rhino", "panda"]
-
+#this is the array where one element will be pass to the zoo poop report as args
+#food
 foods = ["grass", "bamboo", "huge grass"]
-
+#this is the array where one element will be pass to the zoo poop report as args
+#color
 colors = ["green", "brown", "beige"]
+names =["spot", "fido", "champ"]
+
+def zoo_names(animal, name)
+  puts "This is #{name} and it is a #{animal}"
+end
 
 
+#
 def zoo_poop_report(animal, food, color)  
   puts "The %s eats %s and their poop is %s" % [animal, food, color]
 end
