8897f260dd2ed0248664b5fc5848942428444bc4
Genevieve Uy <workgenevieve@gmail.com>
2012-02-13 16:18:29 -0800

    added music_release_dates method

---
+++ automaterepetition.rb
@@ -0,0 +1,17 @@
+artists = ["beck", "prince", "bon iver"]
+
+songs = ["loser", "head", "calgary"]
+
+years = [1995, 1980, 2010]
+
+
+
+def music_release_dates(artist, song, year)
+  puts "%s released %s on %d" % [artist, song, year]
+end
+
+artists.length.times do |x|
+  music_release_dates(artists[x], songs[x], years[x])
+end
+
+  
